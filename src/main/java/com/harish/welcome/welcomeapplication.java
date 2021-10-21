7a14a07ea6e532f0ebb6c3bae75a23fc7e4f78cd
Harisha Prakash <79756878+harish-gownder226@users.noreply.github.com>
2021-10-21 23:32:52 +0200

    Add files via upload

---
+++ src/main/java/com/harish/welcome/welcomeapplication.java
@@ -0,0 +1,14 @@
+package com.harish.welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class welcomeapplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(welcomeapplication.class, args);
+	}
+
+}
